7dda9e3fe1c654d247d918bca89d4e08e347acdb
yongikim <yongi.kim08@gmail.com>
2020-12-04 01:32:16 +0900

    Finish tutorials

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,65 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import { css } from "@emotion/react"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
+import Styles from "./index.module.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default ({ data }) => (
+  <Layout>
+    <h1
+      css={css`
+        display: inline-block;
+        border-bottom: 1px solid;
+      `}
+    >
+      Amazing Pandas Eating Things
+    </h1>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(1 / 4)}
+          `}
+        >
+          <Link
+            to={node.fields.slug}
+            className={Styles.postListTitle}
+          >
+            {node.frontmatter.title}{" "}
+          </Link>
+          <span
+            css={css`
+              color: #bbb
+            `}
+          >
+            - {node.frontmatter.date}
+          </span>
+        </h3>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
---
+++ src/pages/sweet-pandas-eating-sweets.md
@@ -0,0 +1,10 @@
+---
+title: "Sweet Pandas Eating Sweets"
+date: "2017-08-10"
+---
+
+パンダ超かわいい。
+
+超かわいいパンダのビデオあるよ。
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4n0xNbfJLR8" frameborder="0" allowfullscreen></iframe>
